1afa88470905ee10ab985cba0e5acc2cf7d3e6c7
Adrienn Tindall <33725376+adrienntindall@users.noreply.github.com>
2026-07-29 03:24:04 -0400

    zukan_data, ppark narcs

--- src/alph_puzzle.c
+++ src/alph_puzzle.c
@@ -553,14 +553,14 @@ static const OamManagerParam ov110_021E6EA4 = { 0, 128, 0, 32, 0, 128, 0, 32 };
 
 static const OamCharTransferParam ov110_021E6DD0 = { 5, 0, 0, GX_OBJVRAMMODE_CHAR_1D_32K, GX_OBJVRAMMODE_CHAR_1D_32K };
 
-static const ResdatIdList sResdatInfo = {
-    .charRes = NARC_resdat_resdat_00000010_bin,
-    .plttRes = NARC_resdat_resdat_00000011_bin,
-    .cellRes = NARC_resdat_resdat_00000009_bin,
-    .animRes = NARC_resdat_resdat_00000008_bin,
-    .mcelRes = 0xFFFF,
-    .manmRes = 0xFFFF,
-    .headerId = NARC_resdat_resdat_00000074_bin,
+static const u16 sResdatInfo[7] = {
+    resdat_00000010_bin, // GF_GFX_RES_TYPE_CHAR
+    resdat_00000011_bin, // GF_GFX_RES_TYPE_PLTT
+    resdat_00000009_bin, // GF_GFX_RES_TYPE_CELL
+    resdat_00000008_bin, // GF_GFX_RES_TYPE_ANIM
+    0xFFFF,                          // GF_GFX_RES_TYPE_MCEL
+    0xFFFF,                          // GF_GFX_RES_TYPE_MANM
+    resdat_00000074_bin,
 };
 
 static const UnmanagedSpriteTemplate sSpriteTemplates[3] = {
@@ -1159,7 +1159,7 @@ static void AlphPuzzle_FreeBackgroundGraphics(AlphPuzzleData *data) {
 static void AlphPuzzle_InitText(AlphPuzzleData *data) {
     FontID_Alloc(4, data->heapID);
 
-    data->msgData = NewMsgDataFromNarc(MSGDATA_LOAD_DIRECT, NARC_msgdata_msg, NARC_msg_msg_0002_bin, data->heapID);
+    data->msgData = NewMsgDataFromNarc(MSGDATA_LOAD_DIRECT, NARC_msgdata_msg, msg_0002, data->heapID);
     data->messageFormat = MessageFormat_New_Custom(6, 16, data->heapID);
     data->unk30 = String_New(0x80, data->heapID);
 
--- src/berry_pots_app.c
+++ src/berry_pots_app.c
@@ -118,14 +118,14 @@ static const TouchscreenListMenuTemplate sMenuTemplate = {
     31,
     84,
 };
-static const ResdatIdList ov17_02203D78 = {
-    .charRes = NARC_resdat_resdat_00000030_bin,
-    .plttRes = NARC_resdat_resdat_00000031_bin,
-    .cellRes = NARC_resdat_resdat_00000029_bin,
-    .animRes = NARC_resdat_resdat_00000028_bin,
-    .mcelRes = 0xFFFF,
-    .manmRes = 0xFFFF,
-    .headerId = NARC_resdat_resdat_00000079_bin,
+static const u16 ov17_02203D78[7] = {
+    resdat_00000030,
+    resdat_00000031,
+    resdat_00000029,
+    resdat_00000028,
+    0xFFFF,
+    0xFFFF,
+    resdat_00000079,
 };
 static const GraphicsModes ov17_02203D88 = {
     GX_DISPMODE_GRAPHICS,
@@ -1129,7 +1129,7 @@ void BerryPotsApp_FreeNarc(BerryPotsAppData *data) {
 static void BerryPotsApp_SetupText(BerryPotsAppData *data) {
     FontID_Alloc(4, data->heapID);
 
-    data->msgData = NewMsgDataFromNarc(MSGDATA_LOAD_DIRECT, NARC_msgdata_msg, NARC_msg_msg_0248_bin, data->heapID);
+    data->msgData = NewMsgDataFromNarc(MSGDATA_LOAD_DIRECT, NARC_msgdata_msg, msg_0248, data->heapID);
     data->msgFmt = MessageFormat_New_Custom(16, 16, data->heapID);
     data->currentStatusString = String_New(128, data->heapID);
     data->cancelString = NewString_ReadMsgData(data->msgData, msg_0248_00000);
