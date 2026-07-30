4511250607d1589b35bfaff1138cb2a2f208e654
Adrienn Tindall <33725376+adrienntindall@users.noreply.github.com>
2026-07-30 15:55:55 -0400

    gs_opening, item_data, item_icon narcs

--- src/game_clear.c
+++ src/game_clear.c
@@ -316,10 +316,10 @@ static void GameClearSave_InitGraphics(FieldSystem *fieldSystem, GameClearWork *
 
 static void GameClearSave_PrintSaving(FieldSystem *fieldSystem, GameClearWork *env) {
     Options *options = Save_PlayerData_GetOptionsAddr(fieldSystem->saveData);
-    env->windowText = ReadMsgData_NewNarc_NewString(NARC_msgdata_msg, NARC_msg_msg_0040_bin, msg_0040_00015, HEAP_ID_FIELD3);
-    DialogBox_AddWindowToLayer3(env->bgConfig, &env->window, GF_BG_LYR_MAIN_3);
-    DialogBox_LoadFrame(&env->window, options);
-    env->printerId = DialogBox_PrintMessage(&env->window, env->windowText, options, 1);
+    env->windowText = ReadMsgData_NewNarc_NewString(NARC_msgdata_msg, msg_0040, msg_0040_00015, HEAP_ID_FIELD3);
+    sub_0205B514(env->bgConfig, &env->window, 3);
+    sub_0205B564(&env->window, options);
+    env->printerId = sub_0205B5B4(&env->window, env->windowText, options, 1);
     env->waitingIcon = WaitingIcon_New(&env->window, 0x000003e2);
 }
 
@@ -334,7 +334,7 @@ static void sub_02052E70(GameClearWork *env) {
 }
 
 static void GameClearSave_PrintSaveStatus(FieldSystem *fieldSystem, GameClearWork *env, int writeStatus) {
-    MsgData *msgData = NewMsgDataFromNarc(MSGDATA_LOAD_LAZY, NARC_msgdata_msg, NARC_msg_msg_0040_bin, HEAP_ID_FIELD1);
+    MsgData *msgData = NewMsgDataFromNarc(MSGDATA_LOAD_LAZY, NARC_msgdata_msg, msg_0040, HEAP_ID_FIELD1);
 
     if (writeStatus == 2) {
         MessageFormat *msgFmt = MessageFormat_New(HEAP_ID_FIELD1);
